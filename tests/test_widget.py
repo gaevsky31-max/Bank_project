382eab3295cd38553a4a867320fedcc9af291584
Andrew Gaevsky <gaevsky31@gmail.com>
2025-11-22 16:24:36 +0300

    Fix import issues in test_widget module

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,6 +1,8 @@
 """ Тест для widget.py"""
 
-from src.widget import mask_account_card, get_date
+from src.widget import get_date
+from src.widget import mask_account_card
+
 
 def test_mask_account_card() -> None:
     """Тестируем функцию маскировки номера карты/счета"""
@@ -15,11 +17,13 @@ def test_mask_account_card() -> None:
     assert mask_account_card("Счет 64686473678894779589") == "Счет **9589"
     assert mask_account_card("Счет 35383033474447895560") == "Счет **5560"
 
+
 def test_get_date() -> None:
     """Тестируем преобразование формата даты"""
     assert get_date("2024-03-11T02:26:18.671407") == "11.03.2024"
     assert get_date("2023-12-31T23:59:59.999999") == "31.12.2023"
     assert get_date("2024-01-01T00:00:00.000000") == "01.01.2024"
+
 
 if __name__ == "__main__":
     test_mask_account_card()
