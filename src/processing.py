1acfa7da459ae503ef981e3db0846520a6d9d05a
Andrew Gaevsky <gaevsky31@gmail.com>
2025-11-30 18:54:25 +0300

    style: format code with black

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ from typing import Dict
 from typing import List
 
 
-def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -> List[Dict[str, Any]]:
+def filter_by_state(operations: List[Dict[str, Any]], state: str = "EXECUTED") -> List[Dict[str, Any]]:
     """
     Функция фильтрует список операций по статусу
 
@@ -15,7 +15,7 @@ def filter_by_state(operations: List[Dict[str, Any]], state: str = 'EXECUTED') -
     """
     filtered_operations = []
     for operation in operations:
-        if operation.get('state') == state:
+        if operation.get("state") == state:
             filtered_operations.append(operation)
     return filtered_operations
 
@@ -30,4 +30,4 @@ def sort_by_date(operations: List[Dict[str, Any]], reverse: bool = True) -> List
     Returns:
          List[Dict[str, Any]]: Отфильтрованный список операций по дате.
     """
-    return sorted(operations, key=lambda x: x.get('date', ''), reverse=reverse)
+    return sorted(operations, key=lambda x: x.get("date", ""), reverse=reverse)
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -5,57 +5,54 @@ from src.processing import sort_by_date
 
 
 def test_filter_by_state() -> None:
-    """ Тестируем функцию сортировки операций"""
+    """Тестируем функцию сортировки операций"""
     # Тестовые данные:
     test_data = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
     # Тест со статусом по умолчанию (EXECUTED):
     result_default = filter_by_state(test_data)
     assert len(result_default) == 2
     for operation in result_default:
-        assert operation['state'] == 'EXECUTED'
+        assert operation["state"] == "EXECUTED"
 
     # Тест со статусом (CANCELED):
-    result_canceled = filter_by_state(test_data, state='CANCELED')
+    result_canceled = filter_by_state(test_data, state="CANCELED")
     assert len(result_canceled) == 2
     for operation in result_canceled:
-        assert operation['state'] == 'CANCELED'
+        assert operation["state"] == "CANCELED"
+
 
 def test_sort_by_date() -> None:
-    """ Тестируем функцию сортировки даты """
+    """Тестируем функцию сортировки даты"""
     # Тестовые данные:
     test_data = [
-        {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-        {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-        {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-        {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
     # Тест: сортировка по убыванию
     result_desc = sort_by_date(test_data)
     assert len(result_desc) == 4
-    assert result_desc[0]['date'] == '2019-07-03T18:35:29.512364'
-    assert result_desc[1]['date'] == '2018-10-14T08:21:33.419441'
-    assert result_desc[2]['date'] == '2018-09-12T21:27:25.241689'
-    assert result_desc[3]['date'] == '2018-06-30T02:08:58.425572'
+    assert result_desc[0]["date"] == "2019-07-03T18:35:29.512364"
+    assert result_desc[1]["date"] == "2018-10-14T08:21:33.419441"
+    assert result_desc[2]["date"] == "2018-09-12T21:27:25.241689"
+    assert result_desc[3]["date"] == "2018-06-30T02:08:58.425572"
 
     # Тест: сортировка по возрастанию
     result_asc = sort_by_date(test_data, False)
     assert len(result_asc) == 4
-    assert result_asc[0]['date'] == '2018-06-30T02:08:58.425572'
-    assert result_asc[1]['date'] == '2018-09-12T21:27:25.241689'
-    assert result_asc[2]['date'] == '2018-10-14T08:21:33.419441'
-    assert result_asc[3]['date'] == '2019-07-03T18:35:29.512364'
+    assert result_asc[0]["date"] == "2018-06-30T02:08:58.425572"
+    assert result_asc[1]["date"] == "2018-09-12T21:27:25.241689"
+    assert result_asc[2]["date"] == "2018-10-14T08:21:33.419441"
+    assert result_asc[3]["date"] == "2019-07-03T18:35:29.512364"
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     test_filter_by_state()
     test_sort_by_date()
     print("Все тесты пройдены")
-
-
-
